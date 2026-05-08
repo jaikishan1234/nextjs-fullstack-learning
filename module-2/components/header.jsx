1fbce6b1723543149e249465ed7d79a552a31d80
Jai <jaikishannayak@gmail.com>
2026-05-08 16:44:28 +0530

    Mini Project 1 based on routing

---
+++ module-2/components/header.jsx
@@ -0,0 +1,90 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+
+const Header = () => {
+  const pathname = usePathname();
+
+  return (
+    <header className="text-gray-400 bg-gray-900 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            className="w-10 h-10 text-white p-2 bg-purple-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <span className="ml-3 text-xl">Tailblocks</span>
+        </a>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <Link
+            href={"/"}
+            className={`mr-5 px-4 py-2 rounded-lg transition-all ${
+              pathname === "/" ? "bg-purple-500 text-white" : "hover:text-white"
+            }`}
+          >
+            Home
+          </Link>
+
+          <Link
+            href={"/about"}
+            className={`mr-5 px-4 py-2 rounded-lg transition-all ${
+              pathname === "/about"
+                ? "bg-purple-500 text-white"
+                : "hover:text-white"
+            }`}
+          >
+            About
+          </Link>
+
+          <Link
+            href={"/about/user"}
+            className={`mr-5 px-4 py-2 rounded-lg transition-all ${
+              pathname === "/about/user"
+                ? "bg-purple-500 text-white"
+                : "hover:text-white"
+            }`}
+          >
+            User
+          </Link>
+
+          <Link
+            href={"/contact"}
+            className={`mr-5 px-4 py-2 rounded-lg transition-all ${
+              pathname === "/contact"
+                ? "bg-purple-500 text-white"
+                : "hover:text-white"
+            }`}
+          >
+            Contact
+          </Link>
+        </nav>
+        <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          Button
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            className="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
