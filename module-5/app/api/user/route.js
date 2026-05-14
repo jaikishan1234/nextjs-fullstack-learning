fc28367742e8866c519d1e69885d89d16989c5c5
Jai <jaikishannayak@gmail.com>
2026-05-14 15:43:50 +0530

    Cookies

--- module-5/app/api/user/route.js
+++ module-5/app/api/user/route.js
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 
 export async function GET(request) {
@@ -6,14 +6,24 @@ export async function GET(request) {
     // console.log((reqHeaders.get("Authorization")));
 
     const reqHeaders = await headers();
-    console.log(reqHeaders.get("Authorization"));
-    console.log(reqHeaders.get("User-Agent"));
+    // console.log(reqHeaders.get("Authorization"));
+    // console.log(reqHeaders.get("User-Agent"));
 
     // return new Response("Hello from Next.js API route");
+
+    // const username = request.cookies.get("username");
+    // console.log(username);
+
+    const cookieStore = await cookies();
+    cookieStore.set("theme", "dark");
+
+    const theme = cookieStore.get("theme");
+    console.log(theme);
 
     return new Response("<h1>Hello from Next.js API route</h1>", {
         headers: {
             "Content-Type": "text/html",
+            // "set-cookie": "username=abc123",
         }
     });
 }
